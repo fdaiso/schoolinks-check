b7c11813eed150143971244ecbeccabefa88b40a
fdasio <supercooolperson@gmail.com>
2026-08-04 15:39:28 -0500

    Replace fetch API with textarea
    There is no way for a user to get their token
    without using inspect. Since this is targeted
    toward mobile users, fetch is discouraged for a
    somewhat easier method of copy&paste

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
-const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo1NzU4MDY1LCJleHAiOjE3ODU4ODE4NTYsImF1ZCI6IlB5dGhvbkFwaSIsImlzcyI6IkNha2VQSFAiLCJpYXQiOjE3ODU4Njc0NTYsIm9iamVjdF90eXBlIjpbInN0dWRlbnQiXSwiaGFzaF9wYXNzd29yZCI6IkE4M0JDOEYwMkVCOTlCODRCNURCOEM5M0RCRDQyRDUwIiwib2JqZWN0X2lkIjoyNzAzMjIxfQ.fNfdVnqEIfrmM5AQ9-F-UUdElFAWClITNeGLeqyohLk';
 const scheduleContainer = document.querySelector('.scheduleContainer');
-
+const userInput = document.getElementById('userInput');
 let schedule = [{},{},{},{},{},{},{}];
 
 async function getSchedule() {
-    const response = await fetch('https://app.schoolinks.com/api/v1/sl_users/students/0/k12-admins/', {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${TOKEN}`
-        }
-    });
-    console.log(schedule);
-    const data = await response.json();
+    const rawJSON = userInput.value;
+    const data = JSON.parse(rawJSON);
     if (data.detail==="Authentication credentials were not provided.") {
         alert('Error, try signing in again');
         return false;
